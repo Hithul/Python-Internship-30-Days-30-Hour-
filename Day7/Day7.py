89e7b27ee8fabee2bd403098c63c0ec1af69f347
Hithul kannan <65595632+Hithul@users.noreply.github.com>
2020-11-29 23:33:48 +0530

    Add files via upload

---
+++ Day7/Day7.py
@@ -0,0 +1,23 @@
+#task1
+import exercise_module as em
+print(em.my_list)
+em.my_list[0] = 0
+print(em.my_list)
+
+#task2
+import pandas as pd
+series1 = pd.Series([1,2,3,4])
+print(series1)
+
+#task3
+import random as r
+x = r.randint(1, 100)
+print(x)
+y = r.randint(1, 100)
+print(y)
+z = r.randint(1, 100)
+print(z)
+
+# Task 4
+import sys
+sys.path
